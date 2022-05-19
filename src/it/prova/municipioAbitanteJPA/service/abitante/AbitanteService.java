4034a590e1e34a0499f9155d054401de86830ffb
Cristian <cristianma98@gmail.com>
2022-05-19 13:46:20 +0200

    primo commit

---
+++ src/it/prova/municipioAbitanteJPA/service/abitante/AbitanteService.java
@@ -0,0 +1,25 @@
+package it.prova.municipioAbitanteJPA.service.abitante;
+
+import java.util.List;
+
+import it.prova.municipioAbitanteJPA.dao.abitante.AbitanteDAO;
+import it.prova.municipioAbitanteJPA.model.Abitante;
+
+public interface AbitanteService {
+	
+	public List<Abitante> listAllAbitanti() throws Exception;
+
+	public Abitante caricaSingoloAbitante(Long id) throws Exception;
+
+	public void aggiorna(Abitante abitanteInstance) throws Exception;
+
+	public void inserisciNuovo(Abitante abitanteInstance) throws Exception;
+
+	public void rimuovi(Long idAbitanteInstance) throws Exception;
+
+	public List<Abitante> cercaTuttiGliAbitantiConNome(String nome) throws Exception;
+	
+	//per injection
+	public void setAbitanteDAO(AbitanteDAO abitanteDAO);
+
+}
